7d5949ce96e5e2ecabdbe52a2f9106ebdd368a24
Gurkan <hello@gurkz.me>
2023-05-26 17:26:17 +0200

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	quoteProps: 'preserve',
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	proseWrap: 'preserve',
+	endOfLine: 'lf',
+}
